5c4dba6f7fed4e979035dbf082e93b949e70af91
cengel87 <curtisjengel@gmail.com>
2020-03-08 17:45:27 -0600

    base starting point for prog-tracker core

---
+++ Source/ProgTracker.Core/IClientApplicationSetup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+/// <summary>
+/// The core functionality shared between all parts of the ProgTracker application.
+/// </summary>
+namespace ProgTracker.Core
+{
+    /// <summary>
+    /// The entry point into the prog-tracker application used to setup the environment.
+    /// All top level applications should create an instance of this.
+    /// </summary>
+    public interface IClientApplicationSetup
+    {
+    }
+}
